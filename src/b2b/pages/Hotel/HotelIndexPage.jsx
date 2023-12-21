ac3f87f871f108d80d1e3f83dcc1f85d7ae74ab4
Anshid <anshidanu99@gmail.com>
2023-12-21 10:53:19 +0530

    add infinite scroll in tour page

--- src/b2b/pages/Hotel/HotelIndexPage.jsx
+++ src/b2b/pages/Hotel/HotelIndexPage.jsx
@@ -11,7 +11,7 @@ import FilterSection from "./FilterSection";
 import ExtendedVersion from "./ExtendedVersion";
 import CompactVersion from "./CompactVersion";
 import ComparatorVersion from "./ComparatorVersion";
-import InfiniteScroll from "react-infinite-scroll-component";
+import InfiniteScroll from "react-infinite-scroll-component"; 
 import { AiOutlineArrowUp } from "react-icons/ai";
 import { config } from "../../../constants";
 import LoadingBar from "react-top-loading-bar";
--- src/b2b/pages/Tours/ToursHomePage.jsx
+++ src/b2b/pages/Tours/ToursHomePage.jsx
@@ -4,6 +4,9 @@ import { useSelector } from 'react-redux'
 import { config } from '../../../constants'
 import { GoClock } from "react-icons/go";
 import { useNavigate } from 'react-router-dom';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { AiOutlineArrowUp } from "react-icons/ai";
+
 
 
 function ToursHomePage() {
@@ -18,7 +21,11 @@ function ToursHomePage() {
         limit: 20,
         skip: 0,
         totalAttractions: 0,
-    })
+        filteredAttractionCount: 0,
+        responseAttractionLength:0,
+        hasMore: true,
+
+    }) 
 
 
     const fetchAllAttractions = async () => {
@@ -27,31 +34,83 @@ function ToursHomePage() {
             const res = await axios.get(`/b2b/resellers/client/attraction/all?skip=${filters.skip}&limit=${filters.limit}`, {
                 headers: {Authorization: `Bearer ${token}`}
             })
-            setAttractions(res?.data?.attractions)
+            // console.log(res.data, 'response data');
+            // setAttractions(res?.data?.attractions)
+
+            res?.data?.attractions 
+            ? setAttractions((prev)=> [
+                ...new Set([...prev, ...res?.data?.attractions?.data])
+            ])
+            : setAttractions([])
+
             setIsLoading(false)
+
+            setFilters((prev)=>{
+                return {
+                    ...prev,
+                    totalAttractions: res?.data?.attractions?.totalAttractions || 0,
+                    hasMore: res?.data?.attractions?.totalAttractions > attractions?.length,
+                }
+            })
+
         } catch (error) {
             setIsLoading(false)
             console.log(error);
         }
     }
 
+
     useEffect(()=> {
         fetchAllAttractions()
+    }, [filters.skip])
+
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
+    }
+    const [isScrollButtonVisible, setIsScrollButtonVisible] = useState(false)
+
+    useEffect(()=> {
+        window.addEventListener("scroll", ()=> {
+            if(window.scrollY > 100) {
+                setIsScrollButtonVisible(true)
+            } else {
+                setIsScrollButtonVisible(false)
+            }
+        })
     }, [])
 
 
+   
+
   return (
     <div className='pt-2'>
-        <div className='p-10 flex justify-center bg-white w-full h-full'>
-            {
-                !isLoading ? (
+        <div className='py-5 px-32 flex justify-center bg-white w-full h-full'>
                     <div>
-                        {
-                            attractions?.data?.length > 0 ? (
                                 <>
-                                            <div className='grid md:grid-cols-5 sm:grid-cols-2 gap-3'>
+                                <InfiniteScroll
+                                    dataLength={attractions || 0}
+                                    next={()=> {
+                                        if(!isLoading && filters.hasMore) {
+                                            setFilters((prev)=> {
+                                                return {...prev, skip: prev.skip + 1}
+                                            })
+                                        }
+                                    }}
+                                    hasMore={isLoading ? false : filters.hasMore}
+                                    endMessage={
+                                        <p style={{ textAlign: "center"}}>
+                                            {
+                                                !isLoading ? (
+                                                    <p className="text-gray-400 text-sm font-light tracking-tight py-2">Thank you for visiting BE Happy Travel & Tourism</p>
+                                                ) : ""
+                                            }
+                                        </p>
+                                    }
+                                    >
+                                            <div className='grid md:grid-cols-4 sm:grid-cols-2 gap-3'>
                                                 {
-                                                    attractions?.data?.map((ele, index)=> {
+                                                    attractions?.map((ele, index)=> {
+                                                        console.log(ele, 'elements for ');
                                                         return (
                                                             <div key={ele?._id} className='bg-white shadow-md rounded-xl w-full h-[450px] cursor-pointer'
                                                             onClick={()=>{
@@ -63,7 +122,7 @@ function ToursHomePage() {
                                                             </div>
                                                             <div className='p-4'>
                                                                 <div className=' flex justify-center h-14'>
-                                                                    <h1 className='text-md font-extralight text-center max-w-2xl'>{ele?.title}</h1>
+                                                                    <h1 className='text-lg font-extralight text-center max-w-2xl'>{ele?.title}</h1>
                                                                 </div>
                                                                 <div className='flex gap-2 pt-2'>
                                                                     <div className=''>
@@ -100,61 +159,74 @@ function ToursHomePage() {
                                                     })
                                                 }
                                       </div>
+                                      {
+                                        isLoading ? (
+                                            <div className='pt-2'>
+                                                <div className='grid md:grid-cols-4 sm:grid-cols-2 gap-3'>
+                                                {
+                                                    [1,2,3,4]?.map((ele)=>(
+                                                        <div className='bg-white shadow-md rounded-xl w-full h-[450px]'>
+                                                        <div className='w-full h-52 rounded-t-3xl'>
+                                                            <div className='w-full rounded-xl h-full bg-gray-100 animate-pulse'></div>
+                                                        </div>
+                                                        <div className='p-4'>
+                                                            <div className=' flex justify-center'>
+                                                                <div>
+                                                                <h1 className='text-xl font-extralight text-center w-60 h-3 bg-gray-100 animate-pulse'></h1>
+                                                                <div className='pt-2'>
+                                                                <h1 className='text-xl font-extralight text-center w-60 h-3 bg-gray-100 animate-pulse'></h1>
+                                                                </div>
+                                                                </div>
+                                                            </div>
+                                                            <div className='flex gap-2 pt-2'>
+                                                                <div className=''>
+                                                                    <h1 className='bg-gray-100 w-20 h-4 p-1 rounded-lg text-white  text-center font-semibold text-sm'></h1>
+                                                                </div>
+                                                                <div>
+                                                                    <h1 className=' bg-gray-100 rounded-lg h-4 w-32 text-white text-center font-semibold text-sm'></h1>
+                                                                </div>
+                                                            </div>
+                                                            <div className='pt-1'>
+                                                                <h1 className='text-xs font-light text-gray-300 h-2 animate-pulse bg-gray-100 w-24'></h1>
+                                                            </div>
+                                                            <div className='pt-2'>
+                                                                <h1 className='font-bold text-lg bg-gray-100 w-28 animate-pulse h-2'></h1>
+                                                            </div>
+                                                            <div className='pt-2'>
+                                                                <div className='flex justify-between'>
+                                                                    <div>
+                                                                    <h1 className='text-xs font-light text-gray-300 animate-pulse bg-gray-100 w-28 h-3'></h1>
+                                                                    </div>
+                                                                    <div className='flex gap-1'>
+                                                                        <h1 className='bg-gray-100 w-20 h-4 animate-pulse'></h1>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    ))
+                                                }
+                                            
+                                            </div>
+
+                                            </div>
+                                        ) : ""
+                                      }
+                                      </InfiniteScroll>
                                 </>
-                            ) : ""
-                        }
-                        
                     </div>
-                ) : (
-                    <div className='grid md:grid-cols-4 sm:grid-cols-2 gap-3'>
-                        {
-                            [1,2,3,4,5,6,7,8,9]?.map((ele)=>(
-                                <div className='bg-white shadow-md rounded-xl w-full h-[450px]'>
-                                <div className='w-full h-52 rounded-t-3xl'>
-                                    <div className='w-full h-full bg-gray-100 animate-pulse'></div>
-                                </div>
-                                <div className='p-4'>
-                                    <div className=' flex justify-center'>
-                                        <div>
-                                          <h1 className='text-xl font-extralight text-center w-60 h-3 bg-gray-100 animate-pulse'></h1>
-                                        <div className='pt-2'>
-                                          <h1 className='text-xl font-extralight text-center w-60 h-3 bg-gray-100 animate-pulse'></h1>
-                                        </div>
-                                        </div>
-                                    </div>
-                                    <div className='flex gap-2 pt-2'>
-                                        <div className=''>
-                                            <h1 className='bg-gray-100 w-20 h-4 p-1 rounded-lg text-white  text-center font-semibold text-sm'></h1>
-                                        </div>
-                                        <div>
-                                            <h1 className=' bg-gray-100 rounded-lg h-4 w-32 text-white text-center font-semibold text-sm'></h1>
-                                        </div>
-                                    </div>
-                                    <div className='pt-1'>
-                                        <h1 className='text-xs font-light text-gray-300 h-2 animate-pulse bg-gray-100 w-24'></h1>
-                                    </div>
-                                    <div className='pt-2'>
-                                        <h1 className='font-bold text-lg bg-gray-100 w-28 animate-pulse h-2'></h1>
-                                    </div>
-                                    <div className='pt-2'>
-                                        <div className='flex justify-between'>
-                                            <div>
-                                            <h1 className='text-xs font-light text-gray-300 animate-pulse bg-gray-100 w-28 h-3'></h1>
-                                            </div>
-                                            <div className='flex gap-1'>
-                                                <h1 className='bg-gray-100 w-20 h-4 animate-pulse'></h1>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                    {
+                        isScrollButtonVisible ? (
+                            <div
+                                onClick={()=> scrollToTop()}
+                                className="cursor-pointer bg-BEColor fixed bottom-16 text-white h-10 w-10 shadow-mn rounded-full flex justify-center items-center animate-bounce right-12 text-2xl"
+                            >
+                                 <AiOutlineArrowUp />
                             </div>
-                            ))
-                        }
-                     
-                    </div>
-                )
-            }
-          
+                        ) : (
+                            ""
+                        )
+                    }
         </div>
     </div>
   )
