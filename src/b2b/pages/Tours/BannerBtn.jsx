206c0845ef362fb98cb8c2ffd96310e41fc1b652
Anshid <anshidanu99@gmail.com>
2023-12-28 09:15:02 +0530

    added some changes

--- src/b2b/pages/Tours/BannerBtn.jsx
+++ src/b2b/pages/Tours/BannerBtn.jsx
@@ -4,13 +4,13 @@ function BannerBtn({ carouselState, previous, next }) {
     return (
       <div className="my-custom-button-group ">
         <button
-          className='absolute lg:top-[330px] lg:right-20 top-[450px] font-semibold text-white text-[25px] border-2 border-white h-10 w-10 rounded-full'
+          className='absolute lg:top-[330px] lg:right-20 top-[450px] hover:bg-white hover:text-black font-semibold text-white text-[25px] border-2 border-white h-10 w-10 rounded-full'
           onClick={() => previous()}
         >
           &lt;
         </button>
         <button
-          className='absolute lg:top-[330px] lg:right-10 top-[450px]  font-semibold text-white text-[25px] border-2 border-white  h-10 w-10 rounded-full'
+          className='absolute lg:top-[330px] lg:right-10 top-[450px] hover:bg-white hover:text-black font-semibold text-white text-[25px] border-2 border-white  h-10 w-10 rounded-full'
           onClick={() => next()}
         >
           &gt;
