008ace7464ccef52ea9be4e3b7c06b9ff87c6473
rayyan1902 <rayyanakhter1905@gmail.com>
2024-05-07 10:02:45 +0530

    bottom nav updated

--- src/b2b/components/BottomNavigattion/BottomNav.jsx
+++ src/b2b/components/BottomNavigattion/BottomNav.jsx
@@ -30,7 +30,7 @@ function BottomNav({ setSidebarView }) {
             : "block"
         } lg:hidden fixed w-full bottom-0 bg-BEColor text-white h-[48px] flex justify-center items-center`}
       >
-        <div className="flex justify-between px-2 w-full">
+        <div className="flex justify-between px-6 w-full">
           <div className="flex justify-center items-center">
             <div
               className="cursor-pointer"
@@ -92,7 +92,7 @@ function BottomNav({ setSidebarView }) {
           <div className=" flex justify-center items-center relative">
             <div
               className="absolute -top-7 w-[60px] h-[60px] bg-orange-600 rounded-full flex justify-center items-center cursor-pointer"
-              onClick={() => navigate("/")}
+             onClick={() => setMenuModal(true)}
             >
               <div className="flex justify-center items-center text-2xl">
                 <TbLayoutDashboard />
@@ -133,7 +133,7 @@ function BottomNav({ setSidebarView }) {
             </div>
           </div>
 
-          <div className=" flex justify-center items-center">
+          {/* <div className=" flex justify-center items-center">
             <div className="cursor-pointer" onClick={() => setMenuModal(true)}>
               <div className="flex justify-center items-center text-[20px]">
                 <FaBars />
@@ -142,7 +142,7 @@ function BottomNav({ setSidebarView }) {
                 <p className="text-xs">Menu</p>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
       {searchModal && (
@@ -159,7 +159,7 @@ function BottomNav({ setSidebarView }) {
       )}
 
       {menuModal && (
-        <div className="fixed w-full h-full z-50 left-0 top-0 backdrop-blur-xl bg-opacity-30 bg-black">
+        <div className="fixed w-full h-full z-50 left-0 top-0  backdrop-blur-xl bg-opacity-30 bg-black">
           <div className="flex w-full justify-center">
             {/* <div
            onClick={() => setMenuModal(false)}
@@ -167,14 +167,15 @@ function BottomNav({ setSidebarView }) {
          >
            <FaXmark height={40} width={40} />
          </div> */}
-            <div className="bg-white w-full h-full min-h-full  md:mt-[80px] mt-[370px]  md:m-0 text-center overflow-x-auto py-5 rounded-t-2xl shadow-2xl">
+            <div className="w-full h-fit      md:mt-[80px] md:m-0 text-center overflow-x-auto py-5 rounded-t-2xl shadow-2xl">
+           
+              <div className="flex flex-col fixed py-5 bg-white rounded-t-3xl bottom-0 justify-center w-full">
               <p
                 onClick={() => setMenuModal(false)}
                 className="text-lg flex justify-center mb-3 font-semibold underline pb-2"
               >
                 <FaXmark height={20} width={20} className="text-3xl" />
               </p>
-              <div>
                 <p className="text-sm text-left">
                   <div className="flex flex-col gap-3 px-2 w-full">
                     <div className="flex justify-center items-center">
